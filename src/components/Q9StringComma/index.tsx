05c8819c9c577ee3e923f18904028230b23b88b4
Diego <apariciocabral@hotmail.com>
2022-03-25 08:26:14 -0300

    Correção Questão 9

--- src/components/Q9StringComma/index.tsx
+++ src/components/Q9StringComma/index.tsx
@@ -1,11 +1,11 @@
 interface IQ9StringCommaProps {
-    vezes: number;
+    value: number;
 }
 
-const Q9StringComma: React.FC<IQ9StringCommaProps> = ({ vezes }) => {
+const Q9StringComma: React.FC<IQ9StringCommaProps> = ({ value }) => {
     const frase = [];
 
-    for (let contador = 1; contador <= vezes; contador += 1) {
+    for (let contador = 1; contador <= value; contador += 1) {
         frase.push('oi');
     }
 
