5dac0095ab14e496f7762be2955a661f487f4be2
Diego <apariciocabral@hotmail.com>
2022-03-30 15:24:37 -0300

    Questão 44

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Q27Factorial from './components/Q27Factorial';
 import Q28OddEven from './components/Q28OddEven';
 import Q2Uppercase from './components/Q2Uppercase';
 import Q3Lowercase from './components/Q3Lowercase';
+import Q44ChangeColorText from './components/Q44ChangeColorText';
 import Q4StringLength from './components/Q4StringLength';
 import Q5FirstChar from './components/Q5FirstChar';
 import Q6LastChar from './components/Q6LastChar';
@@ -412,7 +413,9 @@ const App: React.FC = () => {
                 qualquer botão, deve-se alterar a cor do texto que está dentro
                 do span
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q44ChangeColorText />
+            </div>
 
             <h2 className="fs-6">
                 45. Crie um componente que tem um <b>span</b> vazio, um input de
---
+++ src/components/Q44ChangeColorText/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+const Q44ChangeColorText: React.FC = () => {
+    const [text, setText] = useState('');
+    const [color, setColor] = useState('');
+
+    return (
+        <>
+            <div>
+                <span style={{ color }}>{text}</span>
+            </div>
+            <div className="mb-2">
+                <input
+                    type="text"
+                    value={text}
+                    onChange={event => setText(event.target.value)}
+                />
+            </div>
+            <div>
+                <button
+                    type="submit"
+                    className="btn btn-danger me-2"
+                    onClick={() => setColor('red')}
+                >
+                    Vermelho
+                </button>
+                <button
+                    type="submit"
+                    className="btn btn-success mx-2"
+                    onClick={() => setColor('green')}
+                >
+                    Verde
+                </button>
+                <button
+                    type="submit"
+                    className="btn btn-primary mx-2"
+                    onClick={() => setColor('blue')}
+                >
+                    Azul
+                </button>
+                <button
+                    type="submit"
+                    className="btn btn-warning mx-2"
+                    onClick={() => setColor('yellow')}
+                >
+                    Amarelo
+                </button>
+                <button
+                    type="submit"
+                    className="btn btn-dark mx-2"
+                    onClick={() => setColor('black')}
+                >
+                    Preto
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Q44ChangeColorText;
