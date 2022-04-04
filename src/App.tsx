aa5dcee97c46057a517818f7ab032f44118e2dea
Diego <apariciocabral@hotmail.com>
2022-04-04 14:20:03 -0300

    Questão 49

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ import Q37ReceiveText from './components/Q37ReceiveText';
 import Q38SumInput from './components/Q38SumInput';
 import Q40LowercaseText from './components/Q40LowercaseText';
 import Q42AddOneLine from './components/Q42AddOneLine';
+import Q49Pokemon from './components/Q49Pokemon';
 
 const App: React.FC = () => {
     return (
@@ -519,7 +520,9 @@ const App: React.FC = () => {
                 nome do pokemon) e retornar a foto do pokemon escolhido no{' '}
                 <b>select</b>. Exibir um loading enquanto procura
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q49Pokemon />
+            </div>
 
             <h2 className="fs-6">
                 50. Crie um componente que tem um um botão, ao clicar no botão o
---
+++ src/components/Q49Pokemon/index.tsx
@@ -0,0 +1,50 @@
+import { useCallback, useState } from 'react';
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'https://pokeapi.co/api/v2/pokemon' });
+
+const Q49Pokemon: React.FC = () => {
+    const [pokemon, setPokemon] = useState('');
+    const [pokemonPic, setPokemonPic] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getPokemon = useCallback(async () => {
+        try {
+            setIsLoading(true);
+            const response = await api.get(`/${pokemon}`);
+            // eslint-disable-next-line camelcase
+            const { front_default } = response.data.sprites;
+            setPokemonPic(front_default);
+        } catch {
+            setPokemon('');
+        } finally {
+            setIsLoading(false);
+        }
+    }, [pokemon]);
+
+    return (
+        <div>
+            {isLoading ? 'Loading' : <img src={pokemonPic} alt="Pokemon" />}
+            <select
+                className="form-select mb-3"
+                onChange={e => setPokemon(e.target.value)}
+            >
+                <option value="">Escolha seu Pokemon</option>
+                <option value="charmander">Charmander</option>
+                <option value="squirtle">Squirtle</option>
+                <option value="caterpie">Caterpie</option>
+                <option value="pidgey">Pidgey</option>
+                <option value="bulbasaur">Bulbasaur</option>
+            </select>
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={getPokemon}
+            >
+                Escolher
+            </button>
+        </div>
+    );
+};
+
+export default Q49Pokemon;
