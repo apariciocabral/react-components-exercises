708652ac3592b1fbeb25261821f37d63b9a76a41
Diego <apariciocabral@hotmail.com>
2022-04-05 13:58:30 -0300

    Questão 30

--- src/components/Q30ZeroToHundred/index.tsx
+++ src/components/Q30ZeroToHundred/index.tsx
@@ -5,10 +5,16 @@ interface IQ30ZeroToHundredProps {
 const Q30ZeroToHundred: React.FC<IQ30ZeroToHundredProps> = ({ value }) => {
     let count = 0;
     for (let i = 0; i <= 100; i += 1) {
-        if (i % 10 === value) {
+        if (i % 10 === value && value > 0 && value < 10) {
             count += 1;
         }
         if (Math.trunc(i / 10) === value) {
+            count += 1;
+        }
+        if (i === 100 && value === 0) {
+            count += 2;
+        }
+        if (i === 100 && value === 1) {
             count += 1;
         }
     }
