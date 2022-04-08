bd0ec0c7b7645d9ed0f31c250772ce8884f1d4c0
Diego <apariciocabral@hotmail.com>
2022-04-08 15:08:30 -0300

    Correção Questão 29

--- src/components/Q29Percentage/index.tsx
+++ src/components/Q29Percentage/index.tsx
@@ -4,7 +4,12 @@ interface IQ29PercentageProps {
 }
 
 const Q29Percentage: React.FC<IQ29PercentageProps> = ({ value, value1 }) => (
-    <p>{(value / value1) * 100}%</p>
+    <p>
+        {value1 === 0
+            ? 'Não é possível realizar esta operação de '
+            : (value / value1) * 100}
+        %
+    </p>
 );
 
 export default Q29Percentage;
